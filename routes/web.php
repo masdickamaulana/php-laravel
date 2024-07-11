6e999219dfc7a1e60f820cc89aa7e9c47b679e71
masdickamaulanaa <dickamaulana2003@gmail.com>
2024-07-11 23:07:21 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/program-studi', \App\Http\Controllers\ProgramStudiController::class);
+Route::get('/sekolah', [App\Http\Controllers\SekolahController::class, 
+'index']);
+Route::get('/fetch-sekolah', [App\Http\Controllers\SekolahController::class, 
+'fetchSekolah']);
